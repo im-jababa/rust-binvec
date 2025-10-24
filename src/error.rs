1c335d935e3bfaebf87e325a19fbc40d6d4ae341
foolkat <cherryjeesoo@gmail.com>
2025-10-24 14:31:28 +0900

    core::error::Error implemented for IndexOutOfBounds error

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,10 @@ pub struct IndexOutOfBounds;
 
 
 impl core::fmt::Display for IndexOutOfBounds {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "index out of bounds")
     }
 }
+
+
+impl core::error::Error for IndexOutOfBounds {}
